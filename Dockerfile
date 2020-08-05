2d398a439d577c547ca4ab54ebacb72708c1db29
ruan <ruanmarins96@gmail.com>
2020-08-05 09:48:16 -0300

    - Adicionando datadog

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,9 @@ FROM python:latest
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
+ENV DD_LOGS_INJECTION=true
+#ENV DD_AGENT_HOST=?
+EXPOSE 8888
+
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from flask import Flask
 import os
 import logging
+import json_log_formatter
 import sys
+from ddtrace import patch_all; patch_all(logging=True)
+from ddtrace import tracer
+from random import randrange
 
 app = Flask(__name__)
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
+formatter = json_log_formatter.JSONFormatter()
+json_handler = logging.StreamHandler(stream=sys.stdout)
+json_handler.setFormatter(formatter)
+logger = logging.getLogger()
+logger.addHandler(json_handler)
+logger.setLevel(logging.INFO)
 
 @app.route("/")
-def hello():
-    logging.info('Hello, World!')
-    return "Flask inside Docker!!"
+def main():
+    logger.info("O caminho de chamada é inválido")
+    return "400 Bad Request", 400
 
+@app.route("/conferir-estoque")
+def confere_estoque():
+    estoque = randrange(101) # Gera número aleatório de 0 a 100
+    logger.info("Estoque disponivel: "+str(estoque)+"%")
+    if estoque > 50:
+        return "200 OK", 200
+    else:
+        logger.error("API de Estoque Indisponivel")
+        return 'API de Estoque Indisponivel', 500
+    
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8888))
     app.run(debug=True,host='0.0.0.0',port=port)
     
